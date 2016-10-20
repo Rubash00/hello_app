c0ecb498b2409e01e83262b66efb6df601a72daf
Ruben <macbookpro@MacBook-Pro-de-MacBook.local>
2016-10-20 15:02:51 -0500

    The First Commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  def hello
+    render text: "Hello World !"
+  end
+  def goodbye
+    render text: "Goodbye World !"
+  end
+  def hola
+    render text: "¡Hola Mundo!"
+  end
+end
